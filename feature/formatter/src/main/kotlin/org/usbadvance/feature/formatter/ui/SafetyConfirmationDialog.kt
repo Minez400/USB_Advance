36f924d9e4e78ac89752aed0d50c5c7fcd7d34f1
USB Advance Developer <developer@usbadvance.org>
2026-09-04 14:47:53 -0300

    fix(formatter): add missing Arrangement import in SafetyConfirmationDialog

--- feature/formatter/src/main/kotlin/org/usbadvance/feature/formatter/ui/SafetyConfirmationDialog.kt
+++ feature/formatter/src/main/kotlin/org/usbadvance/feature/formatter/ui/SafetyConfirmationDialog.kt
@@ -2,6 +2,7 @@ package org.usbadvance.feature.formatter.ui
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
